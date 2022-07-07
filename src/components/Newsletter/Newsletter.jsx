7ab622914d45f10b369bca9b546b63ed5f3370cf
Jude <lovespictures2@gmail.com>
2022-07-07 17:58:20 +0100

    Updated Banner

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
-import "./Newsletter.css";
+import React, { useState } from "react";
+
 import "../Section/Section.css";
+import "./Newsletter.css";
 
 export const Newsletter = () => {
-  return (
-    <div id="Newsletter">Newsletter</div>
-  )
-}
+  const [userEmail, setUserEmail] = useState("");
 
+  const handleFormSubmit = (event) => {
+    // An function to prevent default refresh of the form onload
+    event.preventDefault();
+
+    alert(" Thank you for singing up for my newsletter!");
+  };
+
+  return (
+    <div id="Newsletter">
+      <div className="Section__Container">
+
+        <div className="Newsletter__form-body" name="Newsletter">
+          <h2>Newsletter! </h2>
+          <h2>
+            It only takes a second to be the first to find out about our latest
+            news{" "}
+          </h2>
+
+          <div className="Newsletter__signUp">         
+          <div className="Newsletter__background-img">
+            <img
+              className="bgd__image"
+              src="https://www.radiustheme.com/demo/wordpress/themes/metro/wp-content/uploads/2019/11/banner2233.jpg"
+              alt="#"
+            />
+</div>
+            <fieldset>
+              <form onSubmit={handleFormSubmit} id="Newsletter__form">
+                <div id="Newsletter__form-email">
+                  <label id="Newsletter__form-label" for="email"></label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={userEmail}
+                    onChange={(event) => setUserEmail(event.target.value)}
+                    className="form-control"
+                    placeholder="Your Email Address"
+                    required
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  id="Newsletter__form-submit-Btn"
+                  className="submit-button"
+                >
+                  Submit
+                </button>
+              </form>
+              
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+};
