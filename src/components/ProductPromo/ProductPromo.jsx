e081480efd7f434f913383933e3cabb9c7a30295
Jude <lovespictures2@gmail.com>
2022-08-22 19:22:54 +0100

    Updated ProductPromo CSS & images

--- src/components/ProductPromo/ProductPromo.jsx
+++ src/components/ProductPromo/ProductPromo.jsx
@@ -2,35 +2,43 @@ import React from "react";
 import "./ProductPromo.css";
 // import "../Section/Section.css";
 
-export const ProductPromo = () => {
+// import "./index.css"; /* App.css */
+// import "./Card.css";
+
+export function ProductPromo() {
   return (
     <div id="ProductPromo">
       <div className="ProductPromo__container">
-        <img
-          className="ProductPromo__background-img"
-          src="https://www.radiustheme.com/demo/wordpress/themes/metro/wp-content/uploads/2019/04/banner2.png"
-          alt="#"
-        />
+        <div className="Grid__container-c">
+          <div className="CardR">
+            <img
+              className="ProductPromo__background-img"
+              src="https://www.radiustheme.com/demo/wordpress/themes/metro/wp-content/uploads/2019/04/banner2.png"
+              alt="#"
+            />
+            <img
+              className="ProductPromo__banner-img"
+              src="https://www.radiustheme.com/demo/wordpress/themes/metro/wp-content/uploads/2019/04/banner1.png"
+              alt="#"
+            />
+            <button
+              type="submit"
+              id="ProductPromo__form-submit-Btn"
+              className="BuyNow-button"
+            >
+              Buy Now >
+            </button>
+          </div>
 
-        <img
-          className="ProductPromo__overlay-img"
-          src="https://www.radiustheme.com/demo/wordpress/themes/metro/wp-content/uploads/2019/04/product66.png"
-          alt="#"
-        />
-
-        <img
-          className="ProductPromo__banner-img"
-          src="https://www.radiustheme.com/demo/wordpress/themes/metro/wp-content/uploads/2019/04/banner1.png"
-          alt="#"
-        />
-        <button
-          type="submit"
-          id="ProductPromo__form-submit-Btn"
-          className="submit-button"
-        >
-          Buy Now >
-        </button>
+          <div className="CardS">
+            <img
+              className="ProductPromo__overlay-img"
+              src="https://www.radiustheme.com/demo/wordpress/themes/metro/wp-content/uploads/2019/04/product66.png"
+              alt="#"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
